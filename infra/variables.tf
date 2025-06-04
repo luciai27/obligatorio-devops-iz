a099f04eaa483096287039e83b0ff499f15a5073
Gonzalo Zunino <gonzaloz5@hotmail.com>
2025-06-04 20:12:16 -0300

    fix: se cambia estructura de infra

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,2 +1,8 @@
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
+variable "aws_region" {
+  default = "us-east-1"
+}
+
+variable "environment" {
+  description = "Ambiente a desplegar (dev, test, prod)"
+  type        = string
+}
