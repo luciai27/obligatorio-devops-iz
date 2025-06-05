4e61872ee956ddb3fe95a353867e816f6f77faa4
Gonzalo Zunino <gonzaloz5@hotmail.com>
2025-06-05 20:52:24 -0300

    fix: se agrega infra/backend.tf para que guarde el estado, se hace prueba de segunda pasada

--- infra/variables.tf
+++ infra/variables.tf
@@ -6,10 +6,3 @@ variable "environment" {
   description = "Ambiente a desplegar (dev, test, prod)"
   type        = string
 }
-
-variable "vpc_id" {
-  description = "ID de la VPC donde se desplegarán los recursos"
-  type        = string
-  default = "aws_vpc.main.id"
-  ## Se pone un valor, para que no se recree cada vez que se hace un merge o pr
-}
