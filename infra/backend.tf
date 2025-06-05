95148c5c736a06c35e41f69863dd4563bce16277
Gonzalo Zunino <gonzaloz5@hotmail.com>
2025-06-05 19:11:47 -0300

    fix: se agrega infra/backend.tf para que guarde el estado

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "votting-app-terraform-states"
-#    key            = "infra/${var.environment}.tfstate"
+    key            = "infra/${var.environment}.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-locks"
     encrypt        = true
