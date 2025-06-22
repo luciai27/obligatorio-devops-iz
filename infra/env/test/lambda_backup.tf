2756c5575ce89d1450389c4df8dafc754dedc891
Gonzalo Zunino <gonzaloz5@hotmail.com>
2025-06-22 19:03:12 -0300

    feat: se agrega carpetas con imagenes

---
+++ infra/env/test/lambda_backup.tf
@@ -0,0 +1,19 @@
+resource "aws_lambda_function" "eks_backup" {
+  function_name = "eks-backup-${var.environment}"
+  role          = "arn:aws:iam::186478816830:role/LabRole"
+  package_type  = "Image"
+  image_uri     = "186478816830.dkr.ecr.us-east-1.amazonaws.com/lambda-backup:latest"
+  timeout       = 300
+
+  environment {
+    variables = {
+      CLUSTER_NAME = var.cluster_name
+      BUCKET_NAME  = var.backup_bucket
+      REGION       = "us-east-1"
+    }
+  }
+}
+
+#resource "aws_ecr_repository" "lambda_repo" {
+#  name = "lambda-backup"
+#}
