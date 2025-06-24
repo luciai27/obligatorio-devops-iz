a98df85e4d0b753a5e92c4a6f3f54ecd78552688
Lucia Ibarburu <129332972+luciai69@users.noreply.github.com>
2025-06-24 20:18:10 -0300

    fix: se arregla lambda_backup.tf

--- infra/env/dev/lambda_backup.tf
+++ infra/env/dev/lambda_backup.tf
@@ -1,22 +1,23 @@
-resource "aws_lambda_function" "eks_backup" {
-  function_name = "eks-backup-${var.environment}"
-  role          = "arn:aws:iam::330090896481:role/LabRole"
-  package_type  = "Image"
-  image_uri     = "186478816830.dkr.ecr.us-east-1.amazonaws.com/lambda-backup:latest"
-  timeout       = 300
-
-  environment {
-    variables = {
-      CLUSTER_NAME = var.cluster_name
-      BUCKET_NAME  = var.backup_bucket
-      REGION       = "us-east-1"
-    }
-  }
-}
+#resource "aws_lambda_function" "eks_backup" {
+#  function_name = "eks-backup-${var.environment}"
+#  role          = "arn:aws:iam::330090896481:role/LabRole"
+#  package_type  = "Image"
+#  image_uri     = "186478816830.dkr.ecr.us-east-1.amazonaws.com/lambda-backup:latest"
+#  timeout       = 300
+#
+#  environment {
+#    variables = {
+#      CLUSTER_NAME = var.cluster_name
+#      BUCKET_NAME  = var.backup_bucket
+#      REGION       = "us-east-1"
+#    }
+#  }
+#}
 
 #resource "aws_ecr_repository" "lambda_repo" {
 #  name = "lambda-backup"
 #}
+
 resource "aws_lambda_function" "check_health" {
   function_name = "check-health-${var.environment}"
   role          = "arn:aws:iam::330090896481:role/LabRole"
